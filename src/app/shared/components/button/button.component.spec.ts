663639aac53e26d6d2bcfc687e922d163a75c1b7
thayna-oliveira <oliveira.thayna@outlook.com>
2021-12-21 12:57:06 -0300

    add base tokens

---
+++ src/app/shared/components/button/button.component.spec.ts
@@ -0,0 +1,29 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ButtonComponent } from './button.component';
+
+describe('ButtonComponent', () => {
+  let component: ButtonComponent;
+  let fixture: ComponentFixture<ButtonComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ButtonComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('given an input label when the component then', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { BrandComponent } from './components/brand/brand.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [],
-  exports: [],
+  declarations: [ButtonComponent, BrandComponent],
+  exports: [ButtonComponent, BrandComponent],
 })
 export class SharedModule {}
