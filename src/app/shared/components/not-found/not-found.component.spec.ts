9682c4dd06f7da81fc7e666d1bea3b83427ef689
THAYNA OLIVEIRA DA CONCEICAO <thayna.conceicao@bradesco.com.br>
2021-12-27 16:41:42 -0300

    Add unit testing

--- src/app/shared/components/not-found/not-found.component.spec.ts
+++ src/app/shared/components/not-found/not-found.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { SharedModule } from '@shared/shared.module';
 import { NotFoundComponent } from './not-found.component';
 
@@ -20,5 +21,26 @@ describe('NotFoundComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display a generic message', () => {
+    const description = fixture.debugElement.query(By.css('.description'));
+    expect(description.nativeElement.innerHTML).toBe(
+      'You’re either misspelling the URL or requesting a page that’s no longer here.'
+    );
+  });
+
+  it('should display all elements', () => {
+    component.description = 'Content not found';
+    fixture.detectChanges();
+
+    const image = fixture.debugElement.query(By.css('.image'));
+    expect(image.nativeElement.src).toContain('/assets/images/page-not-found.svg');
+
+    const heading = fixture.debugElement.query(By.css('.title'));
+    expect(heading.componentInstance.title).toBe('Sorry, no results found');
+
+    const description = fixture.debugElement.query(By.css('.description'));
+    expect(description.nativeElement.innerHTML).toBe('Content not found');
   });
 });
