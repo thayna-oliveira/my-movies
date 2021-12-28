0da13eb7893bb1205b7b282e6b0b245e7ef28ad0
THAYNA OLIVEIRA DA CONCEICAO <thayna.conceicao@bradesco.com.br>
2021-12-28 09:51:58 -0300

    bugfix - updating navbar responsiveness

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  @ViewChild('menuToggle', { static: true }) menuToggle: ElementRef;
+
+  public isActive = false;
+
   constructor() {}
+
+  @HostListener('window:scroll')
+  onScroll(): void {
+    this.hideMenu();
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: any) {
+    if (this.menuToggle.nativeElement.contains(event.target)) {
+      if (!this.isActive) {
+        this.showMenu();
+        return;
+      }
+      this.hideMenu();
+    } else {
+      if (this.isActive) {
+        this.hideMenu();
+      }
+    }
+  }
+
+  public showMenu(): void {
+    this.isActive = true;
+  }
+
+  public hideMenu(): void {
+    this.isActive = false;
+  }
 }
